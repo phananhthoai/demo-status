56e8985b237fb7bb86d192f4eda86569a4549f32
khanh <k3nkukhanh@gmail.com>
2024-10-30 11:00:52 +0000

    edit

--- demo/settings.py
+++ demo/settings.py
@@ -146,5 +146,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kenkukhanh@gmail.com'
-EMAIL_HOST_PASSWORD = 'objvolitthfviyum'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER', 'user')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'password')
